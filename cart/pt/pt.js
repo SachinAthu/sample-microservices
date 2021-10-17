1304f16bc409cdab7b713d8c83460f70ccbdfa9d
Sachin Athukorala <sachin2262716@gmail.com>
2021-10-17 09:27:58 +0530

    update

--- cart/pt/pt.js
+++ cart/pt/pt.js
@@ -1,3 +1,5 @@
+// passport, jsonwebtoken
+
 passport.use(new LocalStrategy(
     function(username, password, done) {
         User.findOne({ username: username }, function (err, user) {
@@ -27,3 +29,58 @@ app.post('/login',
 });
 
 app.post('/login', passport.authenticate('local', { failureRedirect: '/login' }));
+
+var jwt = require('jsonwebtoken');
+
+var token = jwt.sign({ foo: 'bar' }, 'shhhhh');
+
+var privateKey = fs.readFileSync('private.key');
+
+var token = jwt.sign({ foo: 'bar' }, privateKey, { algorithm: 'RS256'});
+
+jwt.sign({ foo: 'bar' }, privateKey, { algorithm: 'RS256' }, function(err, token) {
+    console.log(token);
+});
+
+var older_token = jwt.sign({ foo: 'bar', iat: Math.floor(Date.now() / 1000) - 30 }, 'shhhhh');
+
+jwt.sign({
+    exp: Math.floor(Date.now() / 1000) + (60 * 60),
+    data: 'foobar'
+}, 'secret');
+
+jwt.sign({
+    data: 'foobar'
+}, 'secret', { expiresIn: 60 * 60 });
+   
+jwt.sign({
+data: 'foobar'
+}, 'secret', { expiresIn: '1h' });
+
+var decoded = jwt.verify(token, 'shhhhh');
+console.log(decoded.foo)
+ 
+jwt.verify(token, 'shhhhh', function(err, decoded) {
+  console.log(decoded.foo)
+});
+ 
+try {
+  var decoded = jwt.verify(token, 'wrong-secret');
+} catch(err) {}
+ 
+jwt.verify(token, 'wrong-secret', function(err, decoded) {});
+ 
+var cert = fs.readFileSync('public.pem');
+jwt.verify(token, cert, function(err, decoded) {
+  console.log(decoded.foo)
+});
+ 
+var cert = fs.readFileSync('public.pem');
+jwt.verify(token, cert, { audience: 'urn:foo' }, function(err, decoded) {});
+ 
+var cert = fs.readFileSync('public.pem');
+jwt.verify(token, cert, { audience: 'urn:foo', issuer: 'urn:issuer' }, function(err, decoded) {});
+ 
+var cert = fs.readFileSync('public.pem');
+jwt.verify(token, cert, { audience: 'urn:foo', issuer: 'urn:issuer', jwtid: 'jwtid' }, function(err, decoded) {});
+ 
